300cb941cd569640a0f22adb0a86d6fbc1e86418
Alexander Gallego <gallego.alexx@gmail.com>
2021-04-03 21:35:13 -0700

    start_servers.sh: update w/ latest redpanda img

--- start_servers.sh
+++ start_servers.sh
@@ -1,5 +1,5 @@
 # run kafka compatible server
-docker run --name redpanda -p 9092:9092 vectorized/redpanda:latest
+docker run --name redpanda -p 9092:9092 vectorized/redpanda:v21.4.1
 
 # create topics for example
 docker exec -it redpanda rpk topic consume RealServer-request
