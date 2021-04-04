8787dade81487d9e85ef6895f89ca9fd8fb11129
Alexander Gallego <alex@vectorized.io>
2021-04-03 22:07:40 -0700

    start_servers.sh: smaller redpanda footprint

--- start_servers.sh
+++ start_servers.sh
@@ -1,5 +1,20 @@
 # run kafka compatible server
-docker run --name redpanda -p 9092:9092 vectorized/redpanda:v21.4.1
+docker run --rm \
+--name=redpanda \
+-p 9092:9092 \
+"vectorized/redpanda:v21.4.1" redpanda start \
+--smp 1  \
+--memory 1G  \
+--reserve-memory 0M \
+--overprovisioned \
+--node-id 0 \
+--check=false \
+--kafka-addr 0.0.0.0:9092 \
+--advertise-kafka-addr 127.0.0.1:9092 \
+--rpc-addr 0.0.0.0:33145 \
+--advertise-rpc-addr redpanda:33145 \
+-- \
+--kernel-page-cache 1
 
 # create topics for example
 docker exec -it redpanda rpk topic consume RealServer-request
