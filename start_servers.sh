3375c03dab52da6081006dce35119f5bbd6dbec0
Alexander Gallego <alex@vectorized.io>
2021-04-20 08:38:49 -0700

    start_servers.sh: update redpanda
    redpanda no longer needs the kernel-pagecache=1 additional flag.

--- start_servers.sh
+++ start_servers.sh
@@ -2,7 +2,7 @@
 docker run --rm \
 --name=redpanda \
 -p 9092:9092 \
-"vectorized/redpanda:v21.4.1" redpanda start \
+"vectorized/redpanda:v21.4.12" redpanda start \
 --smp 1  \
 --memory 1G  \
 --reserve-memory 0M \
@@ -12,9 +12,7 @@ docker run --rm \
 --kafka-addr 0.0.0.0:9092 \
 --advertise-kafka-addr 127.0.0.1:9092 \
 --rpc-addr 0.0.0.0:33145 \
---advertise-rpc-addr redpanda:33145 \
--- \
---kernel-page-cache 1
+--advertise-rpc-addr redpanda:33145 
 
 # create topics for example
 docker exec -it redpanda rpk topic consume RealServer-request
